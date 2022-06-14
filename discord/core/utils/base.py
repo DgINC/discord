a33b63d91846cae795e21af9b93375e157c2318c
Leonid Kuzin <dg.inc.lcf@gmail.com>
2022-06-14 03:50:46 +0300

    update base.py

--- discord/core/utils/base.py
+++ discord/core/utils/base.py
@@ -1,7 +1,7 @@
 from types import Enum
 
 class METH(str, Enum):
-    PUT: Final[str] = "PUT
+    PUT: Final[str] = "PUT"
     GET: Final[str] = "GET"
     PATCH: Final[str] = "PATCH"
     DELETE: Final[str] = "DELETE"
