75f37b6d7291d19b01587131a62a332e0df9a757
Leonid Kuzin <dg.inc.lcf@gmail.com>
2022-06-14 03:45:05 +0300

    update base.py

--- discord/core/utils/base.py
+++ discord/core/utils/base.py
@@ -1,7 +1,8 @@
-from aiohttp.hdrs import METH_GET, METH_PUT, METH_PATCH, METH_DELETE
+
 
 class METH(str, Enum):
-    METH_PUT
-    METH_GET
-    METH_PATCH
-    METH_DELETE
+    PUT: Final[str] = "PUT
+    GET: Final[str] = "GET"
+    PATCH: Final[str] = "PATCH"
+    DELETE: Final[str] = "DELETE"
+    OPTIONS: Final[str] = "OPTIONS"
